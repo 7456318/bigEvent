e77661c5dcf6ac6c91c61cca5995f8f133a9fad2
fanzhijun <2456080907@qq.com>
2019-10-13 10:50:05 +0800

    新的

---
+++ web_back/js/tool.js
@@ -0,0 +1,7 @@
+
+var base = 'http://localhost:8000';
+
+var LOG_IN = base + '/admin/login';
+var LOG_OUT = base + '/admin/logout'
+var GET_USER = base + '/admin/getuser';
+var ARTICLE_COUNT = base + '/admin/article_count';
